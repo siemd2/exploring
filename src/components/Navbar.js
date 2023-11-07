4717ac371dc67917249750fd925f0c2aa12aec9b
Siem Debesay <siem3500@gmail.com>
2023-11-06 23:08:55 -0700

    Initial commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 function Navbar() {
-  return (
-      <nav className="p-5 bg-gray-900 text-white">
-          <h2 className="text-2xl font-semibold">New Generation High School</h2>
-          {/* Add more navigation items if needed */}
-      </nav>
-  );
-}
-
-export default Navbar;
+    return (
+        <nav className="p-5 bg-gray-900 text-white">
+            <h2 className="text-2xl font-semibold">New Generation High School</h2>
+            {/* Add more navigation items if needed */}
+        </nav>
+    );
+  }
+  
+  export default Navbar;
--- src/components/StudentForm.js
+++ src/components/StudentForm.js
@@ -3,32 +3,51 @@ import React from 'react';
 const StudentForm = ({ onNewStudent }) => {
     const handleSubmit = (event) => {
         event.preventDefault();
-  
+
         // Get the data from the form.
         const formData = new FormData(event.target);
         const newStudent = Object.fromEntries(formData);
-  
+
         // Convert ID and grade to numbers.
         newStudent.id = Number(newStudent.id);
         newStudent.grade = Number(newStudent.grade);
-  
+
         // Call the onNewStudent function passed as a prop.
         onNewStudent(newStudent);
-  
+
         // Reset the form fields.
         event.target.reset();
     };
-  
+
     return (
-        <form onSubmit={handleSubmit} className="space-y-4">
-            <input type="number" name="id" required placeholder="ID #" min="1" className="input" />
-            <input type="text" name="firstName" required placeholder="First Name" className="input" />
-            <input type="text" name="lastName" required placeholder="Last Name" className="input" />
-            <input type="date" name="dateOfBirth" required placeholder="Date of Birth" className="input" />
-            <input type="number" name="grade" required placeholder="Grade" min="1" max="12" className="input" />
-            <button type="submit" className="button">Add Student</button>
-        </form>
-    );
+        <div className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-md">
+            <form onSubmit={handleSubmit} className="space-y-6">
+                <div>
+                    <label htmlFor="id" className="block text-sm font-medium text-gray-700">ID #</label>
+                    <input type="number" id="id" name="id" required min="1" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black" placeholder="Enter ID" />
+                </div>
+                <div>
+                    <label htmlFor="firstName" className="block text-sm font-medium text-gray-700">First Name</label>
+                    <input type="text" id="firstName" name="firstName" required className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black" placeholder="Enter first name" />
+                </div>
+                <div>
+                    <label htmlFor="lastName" className="block text-sm font-medium text-gray-700">Last Name</label>
+                    <input type="text" id="lastName" name="lastName" required className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black" placeholder="Enter last name" />
+                </div>
+                <div>
+                    <label htmlFor="dateOfBirth" className="block text-sm font-medium text-gray-700">Date of Birth</label>
+                    <input type="date" id="dateOfBirth" name="dateOfBirth" required className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black" placeholder="Select date of birth" />
+                </div>
+                <div>
+                    <label htmlFor="grade" className="block text-sm font-medium text-gray-700">Grade</label>
+                    <input type="number" id="grade" name="grade" required min="1" max="12" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black" placeholder="Enter grade" />
+                </div>
+                <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Add Student
+                </button>
+            </form>
+        </div>
+    );    
 };
 
 export default StudentForm;
