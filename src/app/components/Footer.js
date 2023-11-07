223e793132a7ec730843cd428056ded879940800
Siem Debesay <siem3500@gmail.com>
2023-11-06 20:07:44 -0700

    first commit and adding inital files from Issac

---
+++ src/app/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="bg-gray-800 text-white py-6 mt-10">
+            <div className="container mx-auto text-center">
+                <h2 className="text-2xl font-bold mb-2">New Generation High School</h2>
+                <p className="mb-2">123 School Street, Gotham City, 12345</p>
+                <p className="mb-2">Phone: (123) 456-7890</p>
+                <p>Email: contact@newgenerationhs.edu</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
